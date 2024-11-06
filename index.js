d3693106048762c4e1c74f398a32d79a700ac65d
Shreyash458 <shreyashp816@gmail.com>
2024-11-06 15:19:44 +0530

    Updated welcome message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World! This is my first CI/CD pipeline!');
+  res.send('Hello, World! This is an updated version of my CI/CD pipeline!');
 });
 
 app.listen(PORT, () => {
